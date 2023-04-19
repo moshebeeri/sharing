6c1600d26b8664e61a3f64d836da2c268a2bbf3c
moshe beeri <moshe.beeri@gmail.com>
2023-04-19 22:53:46 +0300

    Social login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Button, Col, Row, Container, Form, Navbar, Alert } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import styles from "./Login.module.css"; // Import the CSS module
 
 function Login() {
   //const user = useContext(AuthContext);
@@ -20,11 +22,22 @@ function Login() {
       setErrorMessage(typedError.message);
     }
   };
+  const handleGoogleSignIn = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const authInstance = getAuth();
+      await signInWithPopup(authInstance, provider);
+      // User is signed in successfully, redirect to the home page.
+      navigate("/");
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
 
   return (
     <>
-      <Navbar className="justify-content-between" bg="dark" variant="dark">
-        <Navbar.Brand>Firebase Authentication</Navbar.Brand>
+      <Navbar className="d-flex justify-content-center" bg="transparent" variant="dark">
+        <Navbar.Brand className={styles.navbarTitle}>Authentication</Navbar.Brand>
       </Navbar>
       <Container style={{ maxWidth: "500px", padding: "10px" }} fluid>
         <Form
@@ -76,6 +89,18 @@ function Login() {
             </Col>
           </Row>
         </Form>
+        <Row>
+          <Col xs={12} className="text-center mt-4">
+            <Button variant="outline-primary" onClick={handleGoogleSignIn}>
+              <img
+                src="https://developers.google.com/static/identity/images/g-logo.png"
+                alt="Sign in with Google"
+                style={{ width: '20px', height: '20px', marginRight: '8px' }}
+              />
+              Sign in with Google
+            </Button>
+          </Col>
+        </Row>
       </Container>
     </>
   );
