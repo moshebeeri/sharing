92f164f50bc8ba102c8045ec8ce8a88cb9c6a8ee
moshe beeri <moshe.beeri@gmail.com>
2023-04-20 03:32:54 +0300

    onConfirm hook

--- src/components/EventsViewer.tsx
+++ src/components/EventsViewer.tsx
@@ -1,6 +1,14 @@
 import { Scheduler } from "@aldabil/react-scheduler";
 import { EVENTS } from "./events";
+import type { EventActions, ProcessedEvent } from "@aldabil/react-scheduler/types"
+
+
+const onConfirm = async (event: ProcessedEvent, action:EventActions): Promise<ProcessedEvent> => {
+  console.log(JSON.stringify({ event, action }, null, 2));
+  return event;
+};
 
 export default function EventsViewer() {
-  return <Scheduler events={EVENTS} />;
+
+  return <Scheduler events={EVENTS} onConfirm={ onConfirm } />;
 }
